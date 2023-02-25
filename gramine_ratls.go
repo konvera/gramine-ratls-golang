a3be274e8cef91e228052db4a007eda7f04095dd
lonerapier <lonerapier@proton.me>
2023-02-25 20:21:19 +0000

    add more tests

--- gramine_ratls.go
+++ gramine_ratls.go
@@ -1,4 +1,4 @@
-package main
+package gramine_ratls
 
 // #cgo LDFLAGS: -ldl
 // #include <assert.h>
@@ -11,86 +11,93 @@ package main
 // /* expected SGX measurements in binary form */
 // static char g_expected_mrenclave[32];
 // static char g_expected_mrsigner[32];
-// char g_expected_isv_prod_id[2];
-// char g_expected_isv_svn[2];
+// static char g_expected_isv_prod_id[2];
+// static char g_expected_isv_svn[2];
 //
-// static bool g_verify_mrenclave   = false;
-// static bool g_verify_mrsigner    = false;
+// static bool g_verify_mrenclave = false;
+// static bool g_verify_mrsigner = false;
 // static bool g_verify_isv_prod_id = false;
-// static bool g_verify_isv_svn     = false;
+// static bool g_verify_isv_svn = false;
 //
 // /* RA-TLS: our own callback to verify SGX measurements */
-// int my_verify_measurements(const char* mrenclave, const char* mrsigner,
-//                                   const char* isv_prod_id, const char* isv_svn) {
-//     printf("%s\n", mrsigner);
+// int my_verify_measurements(const char * mrenclave,
+//     const char * mrsigner,
+//     const char * isv_prod_id,
+//     const char * isv_svn) {
 //     assert(mrenclave && mrsigner && isv_prod_id && isv_svn);
 //
 //     if (g_verify_mrenclave &&
-//             memcmp(mrenclave, g_expected_mrenclave, sizeof(g_expected_mrenclave)))
+//         memcmp(mrenclave, g_expected_mrenclave, sizeof(g_expected_mrenclave)))
 //         return -1;
 //
 //     if (g_verify_mrsigner &&
-//             memcmp(mrsigner, g_expected_mrsigner, sizeof(g_expected_mrsigner)))
+//         memcmp(mrsigner, g_expected_mrsigner, sizeof(g_expected_mrsigner)))
 //         return -1;
 //
 //     if (g_verify_isv_prod_id &&
-//             memcmp(isv_prod_id, g_expected_isv_prod_id, sizeof(g_expected_isv_prod_id)))
+//         memcmp(isv_prod_id, g_expected_isv_prod_id, sizeof(g_expected_isv_prod_id)))
 //         return -1;
 //
 //     if (g_verify_isv_svn &&
-//             memcmp(isv_svn, g_expected_isv_svn, sizeof(g_expected_isv_svn)))
+//         memcmp(isv_svn, g_expected_isv_svn, sizeof(g_expected_isv_svn)))
 //         return -1;
 //
 //     return 0;
 // }
 //
-// int ra_tls_verify_callback_der_wrapper(void *f, u_int8_t* der_crt, size_t der_crt_size) {
+// int ra_tls_verify_callback_der_wrapper(void * f, u_int8_t * der_crt, size_t der_crt_size) {
 //
-//     int (*ra_tls_verify_callback_der)(u_int8_t*, size_t);
-//     ra_tls_verify_callback_der = (int (*)(u_int8_t*, size_t))f;
-//     return ra_tls_verify_callback_der(der_crt,der_crt_size);
+//     int( * ra_tls_verify_callback_der)(u_int8_t * , size_t);
+//     ra_tls_verify_callback_der = (int( * )(u_int8_t * , size_t)) f;
+//     return ra_tls_verify_callback_der(der_crt, der_crt_size);
 // }
 //
-// void ra_tls_set_measurement_callback_wrapper(void *f) {
-//     void (*ra_tls_set_measurement_callback)(int (*)(const char*, const char*, const char*, const char*));
-//     ra_tls_set_measurement_callback = (void (*)(int (*)(const char*, const char*, const char*, const char*)))f;
+// void ra_tls_set_measurement_callback_wrapper(void * f) {
+//     void( * ra_tls_set_measurement_callback)(int( * )(const char * ,
+//         const char * ,
+//             const char * ,
+//                 const char * ));
+//     ra_tls_set_measurement_callback = (void( * )(int( * )(const char * ,
+//         const char * ,
+//             const char * ,
+//                 const char * ))) f;
 //     ra_tls_set_measurement_callback(my_verify_measurements);
 // }
 //
 // void set_g_verify_mrenclave(bool val) {
-//	g_verify_mrenclave = val;
+//     g_verify_mrenclave = val;
 // }
 //
 // void set_g_verify_mrsigner(bool val) {
-// 	g_verify_mrsigner = val;
+//     g_verify_mrsigner = val;
 // }
 //
 // void set_g_verify_isv_prod_id(bool val) {
-// 	g_verify_isv_prod_id = val;
+//     g_verify_isv_prod_id = val;
 // }
 //
 // void set_g_verify_isv_svn(bool val) {
-// 	g_verify_isv_svn = val;
+//     g_verify_isv_svn = val;
 // }
 //
-// void set_g_expected(void* dest, void* src, size_t size) {
-//	memcpy(dest, src, size);
+// void set_g_expected(void * dest, void * src, size_t size) {
+//     memcpy(dest, src, size);
 // }
 //
-// void set_g_expected_mrenclave(void* src, size_t size) {
-// 	memcpy(g_expected_mrenclave, src, size);
+// void set_g_expected_mrenclave(void * src, size_t size) {
+//     memcpy(g_expected_mrenclave, src, size);
 // }
 //
-// void set_g_expected_mrsigner(void* src, size_t size) {
-// 	memcpy(g_expected_mrsigner, src, size);
+// void set_g_expected_mrsigner(void * src, size_t size) {
+//     memcpy(g_expected_mrsigner, src, size);
 // }
 //
-// void set_g_expected_isv_prod_id(void* src, size_t size) {
-// 	memcpy(g_expected_isv_prod_id, src, size);
+// void set_g_expected_isv_prod_id(void * src, size_t size) {
+//     memcpy(g_expected_isv_prod_id, src, size);
 // }
 //
-// void set_g_expected_isv_svn(void* src, size_t size) {
-// 	memcpy(g_expected_isv_svn, src, size);
+// void set_g_expected_isv_svn(void * src, size_t size) {
+//     memcpy(g_expected_isv_svn, src, size);
 // }
 import "C"
 
@@ -99,14 +106,19 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
-	"os"
 	"unsafe"
+
+	"github.com/sirupsen/logrus"
 )
 
 var ra_tls_verify_callback_der_f unsafe.Pointer
-var ra_tls_set_measurement_callback_f unsafe.Pointer
 
 func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		DisableTimestamp: true,
+	})
+	logrus.SetLevel(logrus.DebugLevel)
+
 	helper_sgx_urts_lib_name := "libsgx_urts.so"
 	helper_sgx_urts_lib_sym := C.CString("libsgx_urts.so")
 	defer C.free(unsafe.Pointer(helper_sgx_urts_lib_sym))
@@ -134,10 +146,13 @@ func init() {
 	ra_tls_set_measurement_callback := "ra_tls_set_measurement_callback"
 	ra_tls_set_measurement_callback_sym := C.CString(ra_tls_set_measurement_callback)
 	defer C.free(unsafe.Pointer(ra_tls_set_measurement_callback_sym))
-	ra_tls_set_measurement_callback_f = C.dlsym(ra_tls_verify_lib, ra_tls_set_measurement_callback_sym)
+	ra_tls_set_measurement_callback_f := C.dlsym(ra_tls_verify_lib, ra_tls_set_measurement_callback_sym)
 	if ra_tls_set_measurement_callback_f == nil {
 		panic(fmt.Errorf("error resolving %q function", ra_tls_set_measurement_callback))
 	}
+
+	// set verify callback
+	C.ra_tls_set_measurement_callback_wrapper(ra_tls_set_measurement_callback_f)
 }
 
 func set_measurement_verification_args(mrenclave, mrsigner, isv_prod_id, isv_svn []byte) {
@@ -170,10 +185,8 @@ func set_measurement_verification_args(mrenclave, mrsigner, isv_prod_id, isv_svn
 	}
 }
 
+// Verifies RA-TLS attestation x509 DER certificate along with measurement args
 func RATLSVerifyDer(certDER, mrenclave, mrsigner, isv_prod_id, isv_svn []byte) error {
-	// set verify callback
-	C.ra_tls_set_measurement_callback_wrapper(ra_tls_set_measurement_callback_f)
-
 	// check for null for each measurement verification
 	set_measurement_verification_args(mrenclave, mrsigner, isv_prod_id, isv_svn)
 
@@ -183,17 +196,18 @@ func RATLSVerifyDer(certDER, mrenclave, mrsigner, isv_prod_id, isv_svn []byte) e
 
 	ret := C.ra_tls_verify_callback_der_wrapper(ra_tls_verify_callback_der_f, (*C.uchar)(certDER_sym), cert_size)
 
-	fmt.Println("Success: ", ret)
+	logrus.Debug("Result: ", ret)
 	if ret != 0 {
-		return errors.New("error in verifying cert")
+		return ErrorCode(ret)
 	}
 
 	return nil
 }
 
+// Verifies RA-TLS attestation x509 PEM certificate
 func RATLSVerify(cert, mrenclave, mrsigner, isv_prod_id, isv_svn []byte) error {
 	if len(cert) == 0 {
-		return errors.New("empty CERT")
+		return errors.New("Empty PEM certificate!")
 	}
 
 	block, _ := pem.Decode(cert)
@@ -203,17 +217,8 @@ func RATLSVerify(cert, mrenclave, mrsigner, isv_prod_id, isv_svn []byte) error {
 
 	certificate, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
-		return errors.New("failed to create cert")
+		return err
 	}
 
 	return RATLSVerifyDer(certificate.Raw, mrenclave, mrsigner, isv_prod_id, isv_svn)
-}
-
-func main() {
-	certFile, err := os.ReadFile("tls/tlscert.der")
-	if err != nil {
-		panic("error in opening cert file")
-	}
-
-	RATLSVerifyDer(certFile, nil, nil, nil, nil)
 }
---
+++ gramine_ratls_test.go
@@ -0,0 +1,263 @@
+package gramine_ratls
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// RA-TLS Certificate
+var certDer []byte
+var certPem []byte
+
+// RA-TLS measurements args
+var mrenclave []byte
+var mrsigner []byte
+var isv_prod_id []byte
+var isv_svn []byte
+
+func setup() {
+	certFile, err := os.ReadFile("test/tls/tlscert.der")
+	if err != nil {
+		panic("error in opening file")
+	}
+	certDer = certFile
+
+	certFile, err = os.ReadFile("test/tls/tlscert.pem")
+	if err != nil {
+		panic("error in opening file")
+	}
+	certPem = certFile
+
+	mrenclave, _ = hex.DecodeString("fc1cde4925d73140be35d2a3e7de0588188b8bff7622648719bcfe3c7a5bc97f")
+	mrsigner, _ = hex.DecodeString("39a3807530c976387e90a3134ea8bec28bcb4857e79db3ab5eb0e7df6996608e")
+	isv_prod_id = []byte{0, 0}
+	isv_svn, _ = hex.DecodeString("0000")
+
+	fmt.Printf("\033[1;33m%s\033[0m", "> Setup completed\n")
+}
+
+func MockDERCertificate() []byte {
+	ca := &x509.Certificate{
+		SerialNumber: big.NewInt(2019),
+		Subject: pkix.Name{
+			Organization:  []string{"ABC Company, INC."},
+			Country:       []string{"ABC"},
+			Province:      []string{""},
+			Locality:      []string{"XYZ"},
+			StreetAddress: []string{"XYZ ABC"},
+			PostalCode:    []string{"1234"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(10, 0, 0),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	caPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		return nil
+	}
+
+	caBytes, err := x509.CreateCertificate(rand.Reader, ca, ca, &caPrivKey.PublicKey, caPrivKey)
+	if err != nil {
+		return nil
+	}
+
+	return caBytes
+}
+
+func MockPEMCertificate() []byte {
+	return []byte(`-----BEGIN CERTIFICATE-----
+	MIIB0zCCAX2gAwIBAgIJAI/M7BYjwB+uMA0GCSqGSIb3DQEBBQUAMEUxCzAJBgNV
+	BAYTAkFVMRMwEQYDVQQIDApTb21lLVN0YXRlMSEwHwYDVQQKDBhJbnRlcm5ldCBX
+	aWRnaXRzIFB0eSBMdGQwHhcNMTIwOTEyMjE1MjAyWhcNMTUwOTEyMjE1MjAyWjBF
+	MQswCQYDVQQGEwJBVTETMBEGA1UECAwKU29tZS1TdGF0ZTEhMB8GA1UECgwYSW50
+	ZXJuZXQgV2lkZ2l0cyBQdHkgTHRkMFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANLJ
+	hPHhITqQbPklG3ibCVxwGMRfp/v4XqhfdQHdcVfHap6NQ5Wok/4xIA+ui35/MmNa
+	rtNuC+BdZ1tMuVCPFZcCAwEAAaNQME4wHQYDVR0OBBYEFJvKs8RfJaXTH08W+SGv
+	zQyKn0H8MB8GA1UdIwQYMBaAFJvKs8RfJaXTH08W+SGvzQyKn0H8MAwGA1UdEwQF
+	MAMBAf8wDQYJKoZIhvcNAQEFBQADQQBJlffJHybjDGxRMqaRmDhX0+6v02TUKZsW
+	r5QuVbpQhH6u+0UgcW0jp9QwpxoPTLTWGXEWBBBurxFwiCBhkQ+V
+	-----END CERTIFICATE-----
+	`)
+}
+
+func TestMain(m *testing.M) {
+	setup()
+	tests := m.Run()
+	os.Exit(tests)
+}
+
+func Test_RATLSVerifyDer_Certificate(t *testing.T) {
+	t.Run("it should verify certificate with empty measurement args", func(t *testing.T) {
+		err := RATLSVerifyDer(certDer, nil, nil, nil, nil)
+		assert.Nil(t, err)
+	})
+}
+
+func Test_RATLSVerifyDer_CertificateWithMeasurements(t *testing.T) {
+	tests := []struct {
+		name        string
+		mrenclave   []byte
+		mrsigner    []byte
+		isv_prod_id []byte
+		isv_svn     []byte
+	}{
+		{
+			name:        "it should verify mrenclave value",
+			mrenclave:   mrenclave,
+			mrsigner:    nil,
+			isv_prod_id: nil,
+			isv_svn:     nil,
+		},
+		{
+			name:        "it should verify mrsigner value",
+			mrenclave:   nil,
+			mrsigner:    mrsigner,
+			isv_prod_id: nil,
+			isv_svn:     nil,
+		},
+		{
+			name:        "it should verify isv_prod_id value",
+			mrenclave:   nil,
+			mrsigner:    nil,
+			isv_prod_id: isv_prod_id,
+			isv_svn:     nil,
+		},
+		{
+			name:        "it should verify isv_svn value",
+			mrenclave:   nil,
+			mrsigner:    nil,
+			isv_prod_id: nil,
+			isv_svn:     isv_svn,
+		},
+		{
+			name:        "it should verify all measurement args",
+			mrenclave:   mrenclave,
+			mrsigner:    mrsigner,
+			isv_prod_id: isv_prod_id,
+			isv_svn:     isv_svn,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := RATLSVerifyDer(certDer, tt.mrenclave, tt.mrsigner, tt.isv_prod_id, tt.isv_svn)
+			assert.Nil(t, err)
+		})
+	}
+}
+
+func Test_RATLSVerifyDer_IncorrectMeasurements(t *testing.T) {
+	tests := []struct {
+		name        string
+		mrenclave   []byte
+		mrsigner    []byte
+		isv_prod_id []byte
+		isv_svn     []byte
+	}{
+		{
+			name:        "it should throw Certificate verfication failed error due to wrong measurements",
+			mrenclave:   []byte{1, 2},
+			mrsigner:    nil,
+			isv_prod_id: nil,
+			isv_svn:     nil,
+		},
+		{
+			name:        "it should throw Certificate verfication failed error due to wrong measurements",
+			mrenclave:   nil,
+			mrsigner:    []byte{1, 2},
+			isv_prod_id: nil,
+			isv_svn:     nil,
+		},
+		{
+			name:        "it should throw Certificate verfication failed error due to wrong measurements",
+			mrenclave:   nil,
+			mrsigner:    nil,
+			isv_prod_id: []byte{1, 2},
+			isv_svn:     nil,
+		},
+		{
+			name:        "it should throw Certificate verfication failed error due to wrong measurements",
+			mrenclave:   nil,
+			mrsigner:    nil,
+			isv_prod_id: nil,
+			isv_svn:     []byte{1, 2},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := RATLSVerifyDer(certDer, tt.mrenclave, tt.mrsigner, tt.isv_prod_id, tt.isv_svn)
+			assert.Equal(t, err, MBEDTLS_ERR_X509_CERT_VERIFY_FAILED)
+		})
+	}
+}
+
+func Test_RATLSVerifyDer_IncorrectCertificate(t *testing.T) {
+	tests := []struct {
+		cert []byte
+	}{
+		{
+			cert: nil,
+		},
+		{
+			cert: []byte{1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run("it should invalid format error", func(t *testing.T) {
+			err := RATLSVerifyDer(tt.cert, nil, nil, nil, nil)
+			assert.Equal(t, err, MBEDTLS_ERR_X509_INVALID_FORMAT)
+		})
+	}
+}
+
+func Test_RATLSVerifyDer_MockCertificate(t *testing.T) {
+	tests := []struct {
+		cert []byte
+	}{
+		{
+			cert: MockDERCertificate(),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run("it should invalid extension error", func(t *testing.T) {
+			err := RATLSVerifyDer(tt.cert, nil, nil, nil, nil)
+			assert.Equal(t, err, MBEDTLS_ERR_X509_INVALID_EXTENSIONS)
+		})
+	}
+}
+
+func Test_RATLSVerify_Certificate(t *testing.T) {
+	err := RATLSVerify(certPem, nil, nil, nil, nil)
+	assert.Nil(t, err)
+}
+
+func Test_RATLSVerify_CertificateWithMeasurements(t *testing.T) {
+	err := RATLSVerify(certPem, mrenclave, mrsigner, isv_prod_id, isv_svn)
+	assert.Nil(t, err)
+}
+
+func Test_RATLSVerify_NoCertificate(t *testing.T) {
+	err := RATLSVerify(nil, nil, nil, nil, nil)
+	assert.Equal(t, err.Error(), fmt.Sprintf("Empty PEM certificate!"))
+}
+
+func Test_RATLSVerify_InvalidCertificate(t *testing.T) {
+	err := RATLSVerify(MockPEMCertificate(), nil, nil, nil, nil)
+	assert.Equal(t, err.Error(), fmt.Sprintf("failed to decode PEM data"))
+}
